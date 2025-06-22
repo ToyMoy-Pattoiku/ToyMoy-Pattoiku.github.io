2398c041df6727e876a6f4bfac928eafb0b3cddc
Yagi <tatsuya.yagi.toymoy@gmail.com>
2025-06-23 06:41:02 +0900

    index.htmlとindex.tsxを削除

--- index.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import DisasterPreparednessCalculator from '../Downloads/disaster-preparedness-dashboard';
-
-const container = document.getElementById('root');
-const root = createRoot(container!);
-root.render(<DisasterPreparednessCalculator />);
