0f6b53f5cc84c017b4a1394d56cb9f56956dfa23
Yagi <tatsuya.yagi.toymoy@gmail.com>
2025-06-23 06:14:12 +0900

    Appコンポーネントを作成し、DisasterPreparednessCalculatorをインポート

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+// Update the import path below if the file is located elsewhere, for example:
+import DisasterPreparednessCalculator from './disaster-preparedness-dashboard';
+// Or, if the folder name or file name is different, correct it accordingly.
+// import DisasterPreparednessCalculator from './correct-folder/correct-file';
+
+function App() {
+  return (
+    <DisasterPreparednessCalculator />
+  );
+}
+
+export default App;
